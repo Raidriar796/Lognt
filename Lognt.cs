ead745df8e39b00a647487d09828fdf07275ce6f
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2024-07-20 18:25:35 -0700

    Initial release

---
+++ Lognt.cs
@@ -0,0 +1,64 @@
+﻿using HarmonyLib;
+using ResoniteModLoader;            
+using Elements.Core;
+
+namespace Lognt;
+
+public class Lognt : ResoniteMod
+{
+    public override string Name => "Logn't";
+    public override string Author => "Raidriar796";
+    public override string Version => "1.0.0";
+    public override string Link => "https://github.com/Raidriar796";
+    public static ModConfiguration? Config;
+
+    [AutoRegisterConfigKey] public static readonly ModConfigurationKey<bool> Errors =
+        new ModConfigurationKey<bool>("Errors", "Allow errors to be written", () => true);
+
+    [AutoRegisterConfigKey] public static readonly ModConfigurationKey<bool> Logs =
+        new ModConfigurationKey<bool>("Logs", "Allow logs to be written", () => true);
+    
+    [AutoRegisterConfigKey] public static readonly ModConfigurationKey<bool> Warnings =
+        new ModConfigurationKey<bool>("Warnings", "Allow warnings to be written", () => true);
+
+    public override void OnEngineInit()
+    {
+        Harmony harmony = new("net.raidriar796.lognt");
+        Config = GetConfiguration();
+        Config?.Save(true);
+        harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(UniLog), "Error")]
+    private class ErrorPatch
+    {
+        private static bool Prefix()
+        {
+            if (!Config.GetValue(Errors)) return false;
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(UniLog), "Log", [typeof(string), typeof(bool)])]
+    private class LogPatch
+    {
+        private static bool Prefix()
+        {
+            if (!Config.GetValue(Logs)) return false;
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(UniLog), "Warning")]
+    private class WarningPatch
+    {
+        private static bool Prefix()
+        {
+            if (!Config.GetValue(Warnings)) return false;
+
+            return true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Logn't
+ 
+A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/) that allows for disabling all logs, warnings, and errors from being written to disk.
+
+## Notice
+
+This mod is situational. Logs with mods already cannot be submitted for bug reports, but this mod will also prevent the ability to diagnose issues with mods too. If you're experiencing an issue, first and foremost disable all mods. If you're confident or have confirmed an issue is related to a mod, submit bug reports including logs with logging enabled.
+
+For a better general solution, [LogCleaner](https://github.com/art0007i/LogCleaner) is a good option
+
+## Requirements
+- [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader)
+
+## Installation
+1. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
+2. Place [Lognt.dll](https://github.com/Raidriar796/Lognt/releases/latest/download/Lognt.dll) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Resonite logs.
